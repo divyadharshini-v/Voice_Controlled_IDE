80247e135f0718ef46b02014178b696c209842a4
divyadharshini-v <divyadharshinii167@gmail.com>
2025-08-03 15:51:33 +0530

    Work in progress

--- codevoice-backend/app/nlp/transformer_handler.py
+++ codevoice-backend/app/nlp/transformer_handler.py
@@ -1,8 +1,6 @@
 from transformers import pipeline
 
-# Load sentiment analysis model
-sentiment_pipeline = pipeline("sentiment-analysis")
-
 def extract_transformer_sentiment(text):
+    sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english", device=-1)
     result = sentiment_pipeline(text)
-    return result[0]  # returns {'label': 'POSITIVE', 'score': 0.99}
+    return result[0]
